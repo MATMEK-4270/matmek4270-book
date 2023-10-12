84986d34c1d8d0cacdb30af63444a72e3edbd28d
Mikael Mortensen <mikaem@math.uio.no>
2023-10-12 10:47:17 +0200

    Add lecture 10

--- lagrange.py
+++ lagrange.py
@@ -1,8 +1,9 @@
+import numpy as np
 import sympy as sp 
 
 x = sp.Symbol('x')
 
-def Lagrangebasis(xj, x=x):
+def Lagrangebasis(xj, x=x, sympy=True):
     """Construct Lagrange basis function for points in xj
     
     Parameters
@@ -24,7 +25,9 @@ def Lagrangebasis(xj, x=x):
         numer = numert/(x - xj[i])
         denom = Mul(*[(xj[i] - xj[j]) for j in range(n) if i != j])
         ell.append(numer/denom)
-    return ell
+    if sympy:
+        return ell 
+    return [np.polynomial.Polynomial(l.as_poly().all_coeffs()[::-1]) for l in ell]
  
 def Lagrangefunction(u, basis):
     """Return Lagrange polynomial
