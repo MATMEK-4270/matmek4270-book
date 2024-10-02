1d7f1c66351827156c7590058cc13357fd9675b7
Mikael Mortensen <mikaem@math.uio.no>
2024-10-02 20:35:46 +0200

    Update setupenv.sh

--- .devcontainer/setupenv.sh
+++ .devcontainer/setupenv.sh
@@ -2,5 +2,4 @@
 echo 'pyver=$(python -c "import sys;print(sys.version[:(len(str(sys.version_info.major))+len(str(sys.version_info.minor))+1)])")' >> ~/.bashrc
 echo 'source activate matmek4270' >> ~/.bashrc
 echo 'export PYTHONPATH=/opt/conda/envs/matmek4270/lib/python${pyver}/site-packages:$PYTHONPATH' >> ~/.bashrc
-echo 'export PYTHONPATH=/opt/conda/envs/matmek4270/lib/python${pyver}/site-packages:$PYTHONPATH' >> ~/.bash_profile
 source ~/.bashrc
